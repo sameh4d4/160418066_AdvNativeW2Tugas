01004d69796350c988b50b44ba6094c3a49b2323
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-08-19 14:44:00 +0700

    Animation, Android back button

--- app/src/main/java/com/example/avd160418066week2tugas/MainActivity.kt
+++ app/src/main/java/com/example/avd160418066week2tugas/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.avd160418066week2tugas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,null)
     }
 }
