f7d4602bf66f6db2f37abd3a4d7af247b15f91f9
Nicky Setyawan D <71703749+sameh4d4@users.noreply.github.com>
2021-08-19 14:07:50 +0700

    initial commit

---
+++ app/src/main/java/com/example/avd160418066week2tugas/MainFragment.kt
@@ -0,0 +1,46 @@
+package com.example.avd160418066week2tugas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+import kotlin.random.Random
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var n1=randomNumber()
+        var n2=randomNumber()
+        var score=0
+        txtSoal.text="$n1 + $n2"
+        btnSubmit.setOnClickListener {
+            if(txtJawaban.text.toString().toInt()==(n1+n2)){
+                n1=randomNumber()
+                n2=randomNumber()
+                score=score+1
+                txtSoal.text="$n1 + $n2"
+                txtJawaban.setText("")
+            }
+            else{
+                val act=MainFragmentDirections.actionResultFragment(score)
+                Navigation.findNavController(it).navigate(act)
+            }
+        }
+    }
+    fun randomNumber():Int{
+        return (0..100).random()
+    }
+
+}
